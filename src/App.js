34eb368b925259bd6163379bbe50241355775704
Jeshmitha <kalyanamjeshmitha@gmail.com>
2025-10-21 21:38:55 +0530

    Added jwt token for auth-token and added in login route and added logout route.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import routes from "./routes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 //Routes
 app.use("/api",routes);
