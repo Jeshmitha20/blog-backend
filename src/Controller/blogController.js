8078892cea9d57599d931e07d73db1ee15309144
Jeshmitha <kalyanamjeshmitha@gmail.com>
2025-11-23 22:24:33 +0530

    Added route for to fetch blogs.

--- src/Controller/blogController.js
+++ src/Controller/blogController.js
@@ -3,11 +3,21 @@ import Blog from "../models/blog.js";
 
 export const createBlog = async(req, res) => {
     try {
-        
         const blog = new Blog(req.body);
         const savedBlog = await blog.save();
         console.log("blog data is ",savedBlog);
         res.status(200).json({message: "blog has been created successfully"});
+    }catch(err) {
+        res.status(500).json({error: err.message});
+    }
+}
+
+export const getBlogs = async(req, res) => {
+    try {
+        const userId = req.params.id;
+        const blogs = await Blog.find({user: userId});
+        console.log("@@@@@@blogs",blogs);
+        res.status(200).json(blogs);
     }catch(err) {
         res.status(500).json({error: err.message});
     }
